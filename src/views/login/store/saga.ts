225afd9400f1e5eafa57da8c43df0bbf26a46423
lihuan <1968763315@qq.com>
2021-12-27 08:50:09 +0800

    abort

--- src/views/login/store/saga.ts
+++ src/views/login/store/saga.ts
@@ -2,7 +2,7 @@
  * @Author: lihuan
  * @Date: 2021-11-17 21:49:09
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2021-11-18 16:53:33
+ * @LastEditTime: 2021-12-08 17:15:32
  * @Email: 17719495105@163.com
  */
 import { delay, put, takeLatest, call, all } from 'redux-saga/effects';
@@ -15,9 +15,9 @@ import * as actionTypes from './actionTypes';
 import type { ICaptuhaPending } from './types';
 import type { ICaptuha } from '@/api/login/model';
 
-function* fetchCaptuha(actions: ICaptuhaPending): Generator<any, void, ICaptuha> {
+function* fetchCaptuha(actions: ICaptuhaPending) {
   try {
-    const { code } = yield call(getCaptuha, actions.payload.phone);
+    const { code }: ICaptuha = yield call(getCaptuha, actions.payload.phone);
     yield put(fetchCaptuhaSuccess({ code }));
   } catch (err: any) {
     yield put(fetchCaptuhaFail({ msg: err.msg }));
